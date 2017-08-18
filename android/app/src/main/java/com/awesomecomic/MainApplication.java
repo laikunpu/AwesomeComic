b9109c06bb635e8aa5e2c5e6ec79b0cb64728e7e
赖坤浦 <844423705@qq.com>
2017-08-18 15:11:13 +0800

    clean redundant code

--- android/app/src/main/java/com/awesomecomic/MainApplication.java
+++ android/app/src/main/java/com/awesomecomic/MainApplication.java
@@ -7,42 +7,34 @@ import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
-import com.microsoft.codepush.react.CodePush;
 
 import java.util.Arrays;
 import java.util.List;
 
 public class MainApplication extends Application implements ReactApplication {
 
-    private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
-
-        @Override
-        protected String getJSBundleFile() {
-            return CodePush.getJSBundleFile();
-        }
-
-        @Override
-        public boolean getUseDeveloperSupport() {
-            return false;
-        }
-
-        @Override
-        protected List<ReactPackage> getPackages() {
-            return Arrays.<ReactPackage>asList(
-//                    new MainReactPackage(),
-                    new CodePush("ZXb5EkMtUjHinPDjCn1olo5WtjsB7817529e-437e-4caa-bdd9-dfcd4c43cbb9", MainApplication.this, BuildConfig.DEBUG)
-            );
-        }
-    };
-
+  private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
     @Override
-    public ReactNativeHost getReactNativeHost() {
-        return mReactNativeHost;
+    public boolean getUseDeveloperSupport() {
+      return BuildConfig.DEBUG;
     }
 
     @Override
-    public void onCreate() {
-        super.onCreate();
-        SoLoader.init(this, /* native exopackage */ false);
+    protected List<ReactPackage> getPackages() {
+      return Arrays.<ReactPackage>asList(
+          new MainReactPackage()
+      );
     }
+  };
+
+  @Override
+  public ReactNativeHost getReactNativeHost() {
+    return mReactNativeHost;
+  }
+
+  @Override
+  public void onCreate() {
+    super.onCreate();
+    SoLoader.init(this, /* native exopackage */ false);
+  }
 }
--- js/page/App.js
+++ js/page/App.js
@@ -7,14 +7,12 @@ import {Provider} from 'react-redux';
 import configureStore from '../store/configureStore'
 
 import Navigation from './Navigation';
-import codePush from 'react-native-code-push'
 
 const store = configureStore();
 
 export default class App extends Component {
 
     componentDidMount() {
-        codePush.sync()
     }
 
     constructor(props) {
